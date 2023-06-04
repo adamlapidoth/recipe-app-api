7557e8fb698fffb45846d8bec8fdc5063bbe2108
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-04 11:42:54 +0300

    added sample tests

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int) -> int:
+    """
+    Adds x and y and return results
+    :param x: An integer
+    :param y: An integer
+    :return: The result
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """
+    Subtract x from y and return result.
+    :param x: An integer
+    :param y: An integer
+    :return: The result
+    """
+    return y - x
