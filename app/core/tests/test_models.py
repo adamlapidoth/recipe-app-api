19d55d01e994e1b36eaeb96d7dbccdbdbda8eb43
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-11 11:12:23 +0300

    created tag model + migrations + unittest

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,6 +11,12 @@ from core import models
 TEST_EMAIL = "test@example.com"
 
 
+def create_user(email: str = "user@example.com",
+                password: str = "testpass123"):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """Test models"""
 
@@ -66,3 +72,10 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
