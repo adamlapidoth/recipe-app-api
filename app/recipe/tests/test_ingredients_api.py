3b870d0e9dc3fed7ecc47615c4af2de425b460c2
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-12 15:07:30 +0300

    added capability to delete ingredient + unittests

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -19,7 +19,7 @@ def detail_url(ingredient_id):
 
 
 def create_user(
-    email: str = "user@example.com", password: str = "testpass123"
+        email: str = "user@example.com", password: str = "testpass123"
 ):
     return get_user_model().objects.create_user(email=email, password=password)
 
@@ -81,3 +81,14 @@ class PrivateIngredientApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload["name"])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name="Lettuce")
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -59,7 +59,8 @@ class TagViewSet(
 
 
 class IngredientViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+    mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin,
+    viewsets.GenericViewSet
 ):
     """Manage ingredients in the database"""
 
