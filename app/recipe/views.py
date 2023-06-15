e9de182c27d0e496fd548d1bfb488d9977e5808c
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-15 14:52:41 +0300

    used black on project

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -33,12 +33,12 @@ from recipe import serializers
             OpenApiParameter(
                 "tags",
                 OpenApiTypes.STR,
-                description="Comma separated list of tag IDs to filter"
+                description="Comma separated list of tag IDs to filter",
             ),
             OpenApiParameter(
                 "ingredients",
                 OpenApiTypes.STR,
-                description="Comma separated list of ingredients IDs to filter"
+                description="Comma separated list of ingredients IDs to filter",
             ),
         ]
     )
@@ -71,9 +71,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_id = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_id)
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by("-id").distinct()
+        return (
+            queryset.filter(user=self.request.user).order_by("-id").distinct()
+        )
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
@@ -133,9 +133,11 @@ class BaseRecipeAttrViewSet(
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by("-name").distinct()
+        return (
+            queryset.filter(user=self.request.user)
+            .order_by("-name")
+            .distinct()
+        )
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
